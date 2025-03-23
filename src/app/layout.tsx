f774e84f0b18a98fb828dfad61c03e7b48c14897
Adeline de Gaulejac <adelinedegaulejac@gmail.com>
2025-03-23 21:47:49 +0100

    fixed styled components issue in SSR with StyledComponentsRegistry and babel plugin styled components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import StyledComponentsRegistry from '../lib/registry';
 
 export const metadata: Metadata = {
   title: 'Zara Mobile Catalog | Buy Apple and Samsung smartphones',
@@ -14,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <StyledComponentsRegistry>
+          {children}
+        </StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
