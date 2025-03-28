527977a4cf6a6ed5c7b2f8c464a1b0a04abb5abc
Adeline de Gaulejac <adelinedegaulejac@gmail.com>
2025-03-28 15:41:49 +0100

    added await params in mobiles/[mobileId]/page.tsx to fix console warning caused by the new async request API introduced in Next.js v15 where params and serachParams are treated as async

--- src/app/mobiles/[mobileId]/page.tsx
+++ src/app/mobiles/[mobileId]/page.tsx
@@ -1,10 +1,10 @@
 import { fetchMobile } from '@/services/search';
-import { MobilePageProps } from '@/interfaces/mobilePageProps';
 import { notFound } from 'next/navigation';
 import MobileDetail from '@/components/MobileDetail';
 
-export default async function MobileDetailPage({ params }: MobilePageProps) {
-  const mobile = await fetchMobile(params.mobileId);
+export default async function MobileDetailPage(props: { params: { mobileId: string } }) {
+  const { mobileId } = await props.params;
+  const mobile = await fetchMobile(mobileId);
 
   if (!mobile) {
     return notFound();
